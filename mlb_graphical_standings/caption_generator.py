d64c11a722136f0482432fe44789763ed65bce55
tunasplam <tunasplam@gmail.com>
2025-02-22 16:37:40 -0800

    now loading env vars here since this is the only place that uses them.

--- mlb_graphical_standings/caption_generator.py
+++ mlb_graphical_standings/caption_generator.py
@@ -1,27 +1,32 @@
 """Anything regarding using GPT to generate captions is done here.
 """
+from os import environ
+from pathlib import Path
+
+from dotenv import load_dotenv
 from openai import OpenAI
 import pandas as pd
 
+# check if config file exists and load
+status = load_dotenv(Path(environ['HOME']) / Path('.config/mlb-graphical-standings/.env'))
+if not status:
+    raise RuntimeError('No config file found. Make sure you have a .env file in ~/.config/mlb-graphical-standings. Check the README!')
+
 client = OpenAI()
 
-def generate_caption(df: pd.DataFrame) -> str:
-
-    with open('prompt.txt', 'r') as f:
-        prompt = f.read()
-
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a cool and smart friend who thoroughly enjoys baseball. No emojis and no hashtags. Be professional but witty."
-                },
-                {
-                    "role": "user",
-                    "content": f"{prompt} Data: {str(df)}"
-                }
-            ]
-        )
+def generate_caption(df: pd.DataFrame, prompt: str) -> str:
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a cool and smart friend who thoroughly enjoys baseball. No emojis and no hashtags. Be professional but witty."
+            },
+            {
+                "role": "user",
+                "content": f"{prompt} Data: {str(df)}"
+            }
+        ]
+    )
 
     return response.choices[0].message.content
